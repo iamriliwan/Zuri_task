5ba848cb5ad1fd5e185afeaba786f69b82c552db
Riliwan Balogun <riliwan.balogun99@gmail.com>
2021-04-20 00:15:52 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+const fetch = require("node-fetch")
+const http = require("http")
+let fs = require('fs')
+
+fetch('http://jsonplaceholder.typicode.com/posts/')
+    .then(response => response.json())
+    .then(json => {
+        fs.writeFile('result/post.json', JSON.stringify(json), ()=>{
+            // if (err) throw err;
+            console.log('file created')
+        })
+    })
+
+        
