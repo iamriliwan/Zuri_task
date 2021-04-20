b0d7b190ad8debe31f9b7b76d64c65794cfbbd50
Riliwan Balogun <riliwan.balogun99@gmail.com>
2021-04-20 13:31:14 +0100

    Refactored and add comment to code

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
+// Imported fetch, http and the fs module
 const fetch = require("node-fetch")
 const http = require("http")
 let fs = require('fs')
 
+// Fetched data from the dummy api http://jsonplaceholder.typicode.com/posts/
 fetch('http://jsonplaceholder.typicode.com/posts/')
     .then(response => response.json())
     .then(json => {
-        fs.writeFile('result/post.json', JSON.stringify(json), ()=>{
-            // if (err) throw err;
+        // Wrote the the posts.json file and coverted to a string
+        fs.writeFile('result/posts.json', JSON.stringify(json), ()=>{
             console.log('file created')
         })
     })
